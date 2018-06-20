02366125094fa17e5c554e679a2bcec0792b22dd
Juan Carlos Moya <juancar.moygar@gmail.com>
2018-06-19 19:35:49 -0600

    Iniciando nuevo proyecto, creando Puestos

---
+++ ControlSistemaSeguridad/Model/DBColegio_context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ControlSistemaSeguridad.Model
+{
+    public class DBColegio_context : DbContext
+    {
+        public DbSet<Puesto> Puestos { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Puesto>()
+                .ToTable("Puestos")
+                .HasKey(p => new { p.PuestoId })
+                .Property(d => d.Descripcion)
+                .IsRequired()
+                .HasMaxLength(256)
+                ;
+
+        }
+    }
+}
---
+++ ControlSistemaSeguridad/Model/Puesto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlSistemaSeguridad.Model
+{
+    public class Puesto
+    {
+        public int PuestoId { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
